abccf0ce30930a5e536f11645e8333ea49a497de
Emilian Peev <epeevs@codeaurora.org>
2014-01-13 23:48:01 -0800

    Revert "QCamera2: Adds memory pools for stream buffers"
    This reverts commit 3dde7571c2323e043ad0833d1985c894a5f6f7ad.

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -1459,10 +1459,7 @@ QCameraMemory *QCamera2HardwareInterface::allocateStreamBuf(cam_stream_type_t st
     case CAM_STREAM_TYPE_PREVIEW:
         {
             if (isNoDisplayMode()) {
-                mem = new QCameraStreamMemory(mGetMemory,
-                                              bCachedMem,
-                                              &m_memoryPool,
-                                              stream_type);
+                mem = new QCameraStreamMemory(mGetMemory, bCachedMem);
             } else {
                 cam_dimension_t dim;
                 QCameraGrallocMemory *grallocMemory =
@@ -1503,10 +1500,7 @@ QCameraMemory *QCamera2HardwareInterface::allocateStreamBuf(cam_stream_type_t st
     case CAM_STREAM_TYPE_RAW:
     case CAM_STREAM_TYPE_METADATA:
     case CAM_STREAM_TYPE_OFFLINE_PROC:
-        mem = new QCameraStreamMemory(mGetMemory,
-                                      bCachedMem,
-                                      &m_memoryPool,
-                                      stream_type);
+        mem = new QCameraStreamMemory(mGetMemory, bCachedMem);
         break;
     case CAM_STREAM_TYPE_VIDEO:
         {
