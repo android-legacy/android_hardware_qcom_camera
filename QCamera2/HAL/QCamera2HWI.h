002716a9861340b94cb422cd5f14a2fc77ac6b34
Mukund Mittal <mukundm@codeaurora.org>
2014-01-17 14:46:11 +0530

    Camera: Block CancelAF till HAL receives AF event.
    Focus distances should not be updated after CancelAF. But due
    to timing issues, sometimes we may receive AF event after
    Cancel AF is issued. This will result if testCancelAutoFocus
    CTS failure.
    To avoid such scenarios, make Cancel AF wait for the focus event
    if AF is in progress.
    Change-Id: Iba641487c8f31de2d118fbc7dc3ed040c9d34601

--- QCamera2/HAL/QCamera2HWI.h
+++ QCamera2/HAL/QCamera2HWI.h
@@ -305,6 +305,7 @@ private:
 
     bool needDebugFps();
     bool isCACEnabled();
+    bool isAFRunning();
     bool needReprocess();
     bool needRotationReprocess();
     bool needScaleReprocess();
