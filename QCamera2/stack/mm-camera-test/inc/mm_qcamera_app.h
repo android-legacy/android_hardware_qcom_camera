ce2ddb43a907efba16f3e34f3bfb735b55847d56
erikcas <erikcas1972@gmail.com>
2015-04-14 07:55:58 +0200

    Revert "QCamera2: Pass Pix size to jpeg open"
    This reverts commit c4fa027b496040b306b1f08edc1422d812ec1a6a.

--- QCamera2/stack/mm-camera-test/inc/mm_qcamera_app.h
+++ QCamera2/stack/mm-camera-test/inc/mm_qcamera_app.h
@@ -254,8 +254,7 @@ typedef struct {
 
   uint8_t (*get_num_of_cameras) ();
   mm_camera_vtbl_t *(*mm_camera_open) (uint8_t camera_idx);
-  uint32_t (*jpeg_open) (mm_jpeg_ops_t *ops, mm_dimension picture_size);
-
+  uint32_t (*jpeg_open) (mm_jpeg_ops_t *ops);
 } hal_interface_lib_t;
 
 typedef struct {
